c73fb473809ca83148b75c95989404d77d04d6c0
TesifonJ <coul1212@gmail.com>
2023-11-16 20:15:29 +0100

    Instalado tailwindcss, añadido navbar y logo, login funcional

--- src/app/components/shared/login-routed/login-routed.component.ts
+++ src/app/components/shared/login-routed/login-routed.component.ts
@@ -1,9 +1,67 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CryptoService } from 'src/app/services/crypto.service';
+import { SessionAjaxService } from 'src/app/services/session.ajax.service';
 
 @Component({
   templateUrl: './login-routed.component.html',
   styleUrls: ['./login-routed.component.scss']
 })
 export class LoginRoutedComponent {
+  loginForm: FormGroup;
+  status: HttpErrorResponse | null = null;
+
+  constructor(
+    private fb: FormBuilder,
+    private oSessionService: SessionAjaxService,
+
+    private oRouter: Router,
+    private oCryptoService: CryptoService
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.oSessionService.login(this.loginForm.value.username, this.oCryptoService.getSHA256(this.loginForm.value.password)).subscribe({
+        next: (data: string) => {
+          this.oSessionService.setToken(data);
+          this.oSessionService.emit({ type: 'login' });
+          //this.oMatSnackBar.open("Loggin successfull.", '', { duration: 2000 });
+          this.oRouter.navigate(['/home']);
+        },
+        error: (error: HttpErrorResponse) => {
+          this.status = error;
+         // this.oMatSnackBar.open("Error in loggin operation.", '', { duration: 2000 });
+        }
+      });
+    }
+  }
+
+  onReset() {
+    this.loginForm.reset();
+  }
+
+  loginAdmin() {
+    this.loginForm.setValue({
+      username: 'ucarter',
+      password: 'foxforum'
+    })
+  }
+
+  loginUser() {
+    this.loginForm.setValue({
+      username: 'pablomarmol',
+      password: 'foxforum'
+    })
+  }
 
 }
